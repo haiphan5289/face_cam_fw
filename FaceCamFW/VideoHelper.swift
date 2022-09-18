9cd7fdac63acc9767f7b38bbb0916c2069027b81
haiphan5289 <haiphan5289@gmail.com>
2022-09-18 15:10:00 +0700

    remove viddeo ranglser

--- FaceCamFW/VideoHelper.swift
+++ FaceCamFW/VideoHelper.swift
@@ -1,32 +1,32 @@
+////
+////  VideoHelper.swift
+////  xrecorder
+////
+////  Created by Huy on 15/03/2022
+////
 //
-//  VideoHelper.swift
-//  xrecorder
+//import UIKit
+//import AVFoundation
 //
-//  Created by Huy on 15/03/2022
+//public class VideoHelper: NSObject {
 //
-
-import UIKit
-import AVFoundation
-
-public class VideoHelper: NSObject {
-
-    static public func thumbnailFromVideo(videoUrl: URL, time: CMTime) -> UIImage{
-        let asset: AVAsset = AVAsset(url: videoUrl) as AVAsset
-        let imgGenerator = AVAssetImageGenerator(asset: asset)
-        imgGenerator.appliesPreferredTrackTransform = true
-        do{
-            let cgImage = try imgGenerator.copyCGImage(at: time, actualTime: nil)
-            let uiImage = UIImage(cgImage: cgImage)
-            return uiImage
-        }catch{
-            
-        }
-        return UIImage()
-    }
-    
-    static public func videoDuration(videoURL: URL) -> Float64 {
-        let source = AVURLAsset(url: videoURL)
-        return CMTimeGetSeconds(source.duration)
-    }
-    
-}
+//    static public func thumbnailFromVideo(videoUrl: URL, time: CMTime) -> UIImage{
+//        let asset: AVAsset = AVAsset(url: videoUrl) as AVAsset
+//        let imgGenerator = AVAssetImageGenerator(asset: asset)
+//        imgGenerator.appliesPreferredTrackTransform = true
+//        do{
+//            let cgImage = try imgGenerator.copyCGImage(at: time, actualTime: nil)
+//            let uiImage = UIImage(cgImage: cgImage)
+//            return uiImage
+//        }catch{
+//
+//        }
+//        return UIImage()
+//    }
+//
+//    static public func videoDuration(videoURL: URL) -> Float64 {
+//        let source = AVURLAsset(url: videoURL)
+//        return CMTimeGetSeconds(source.duration)
+//    }
+//
+//}
